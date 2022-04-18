3c0b3ef26c35f8d84dbc17f72adf862e57b323c1
venegas55 <venegas55@me.com>
2022-04-18 17:48:01 -0500

    Follow up Week 1

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,6 @@
+// node main.js
+//no me queda aún claro en qué momento main manda a llamar a logger...
+const logger = require('./logger')
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
